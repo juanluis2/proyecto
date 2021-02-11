c6c3d16d9f9050434b770c34f94902e0e19150b2
Juan Medina <juan221094@gmail.com>
2021-02-11 15:27:37 -0500

    No mostrar en otras ramas

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react'
 
 const Index = () => {
-    return
+    return (
     <>
         <h1> Recuperado... </h1>
         <h2>Este es un subtitulo</h2>
         <p>Quiero ver los cambios</p>
         <p>Otra cosa</p>
         <p>Ahora si vez mis cambios</p>
-    </>         
+
+        <form>
+            <input type="text" placeholder="provando rama.." />
+            <button > Enviar </button>
+            <p>Esto se vera solo en el feature</p>
+        </form>
+    </>     
+    )    
 }
 
 export default Index
