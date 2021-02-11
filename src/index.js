ccff8f9eebe64e724077db92e278f93919475ce2
Juan Medina <juan221094@gmail.com>
2021-02-11 15:02:22 -0500

    Se elimino la rama developer

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const Index = () => {
         <h1> Recuperado... </h1>
         <h2>Este es un subtitulo</h2>
         <p>Quiero ver los cambios</p>
+        <p>Otra cosa</p>
+        <p>Ahora si vez mis cambios</p>
     </>         
 }
 
