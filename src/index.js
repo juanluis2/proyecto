abe018cda1468bd47ace30c3e5190d1dd8a533e4
Juan Medina <juan221094@gmail.com>
2021-02-11 11:15:14 -0500

    Proyecto funcional

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-<h1>Nuevo</h1>
+import React from 'react'
+
+const Index = () => {
+    return
+    <>
+        <h1> Recuperado... </h1>
+        <h2>Este es un subtitulo</h2>
+    </>         
+}
+
+export default Index
