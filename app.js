7c9ee3d891559aaed8a8d10912aa81796bdc48d3
Juan Medina <juan221094@gmail.com>
2021-02-11 11:07:39 -0500

    Salvando datos

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const app = () => {
 return
-    <h1>hola</h1>
+    <>
+        <h1>hola</h1>
+        <input type="text" placeholder="Ingrese su nombre" />
+        <input type="textArea" placeholder="Ingrese una descripción" />
+        <button type="reset"> Limpiar campos</button>
+    </>
 }
 
 export default app
